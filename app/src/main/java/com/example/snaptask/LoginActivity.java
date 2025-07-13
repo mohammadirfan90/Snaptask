cd6d26ff21db8187e4133ade5d220ce2eef21471
Mohammad Irfan <irfanmd.im97@gmail.com>
2025-07-13 10:50:13 +0600

    Firebase setup done

--- app/src/main/java/com/example/snaptask/LoginActivity.java
+++ app/src/main/java/com/example/snaptask/LoginActivity.java
@@ -1,20 +1,20 @@
 package com.example.snaptask;
 
 import android.os.Bundle;
-import com.example.snaptask.R;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity2 extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.login_activity);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -22,3 +22,4 @@ public class MainActivity2 extends AppCompatActivity {
         });
     }
 }
+
--- app/src/main/java/com/example/snaptask/SignUpActivity.java
+++ app/src/main/java/com/example/snaptask/SignUpActivity.java
@@ -1,20 +1,20 @@
 package com.example.snaptask;
 
 import android.os.Bundle;
-import com.example.snaptask.R;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.signup_activity);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -22,4 +22,3 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
-
